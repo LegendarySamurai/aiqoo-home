84ec5163e50598252bd910d5cc162cd8c5bcde67
Igor Goncharenko <igor.goncharenko86@gmail.com>
2020-02-23 16:44:33 +0200

    Aiqoo home desktop done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,33 @@
 ﻿(function () {
-    var CallToActionText = document.getElementById("CallToActionText");
-    if (CallToActionText) {
-        window.onscroll = function (e) {
-            var scrollPosition = window.pageYOffset;
-            if (scrollPosition > 500) {
-                CallToActionText.classList.add("float");
-            }
-        }
-    }
+	var CallToActionText = document.getElementById('CallToActionText');
+	if (CallToActionText) {
+		window.onscroll = function (e) {
+			var scrollPosition = window.pageYOffset;
+			if (scrollPosition > 500) {
+				CallToActionText.classList.add('float');
+			}
+		}
+	}
 })();
+
+
+// Igor created 23 February 2020
+const header = document.querySelector('.header');
+let headerClientRect = header.getBoundingClientRect();
+header.classList.remove('sticky');
+
+const makeHeaderSticky = () => {
+	if (window.pageYOffset >= headerClientRect.bottom) {
+		header.classList.add('sticky');
+		headerClientRect = header.getBoundingClientRect();
+
+	}
+	else if (window.pageYOffset < headerClientRect.bottom) {
+		header.classList.remove('sticky');
+		headerClientRect = header.getBoundingClientRect();
+	}
+};
+
+window.addEventListener('scroll', function () {
+	makeHeaderSticky();
+});
