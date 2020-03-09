c4117893b2305c86d2fee9dcfedff29ee597b0ce
Igor Goncharenko <igor.goncharenko86@gmail.com>
2020-03-09 19:54:25 +0200

    landing sign up almost done

---
+++ assets/js/landing-sign-up.js
@@ -0,0 +1,17 @@
+// const isMobile = window.innerWidth < 800;
+//
+// const letsGoBtn = document.querySelector('.lets-go-btn');
+// const formWrapper = document.querySelector('.form-wrapper');
+// let popupVisible = false;
+//
+//
+// letsGoBtn.addEventListener('click', function() {
+//
+// 	if (isMobile) {
+// 		formWrapper.classList.add('is-active');
+// 		popupVisible = true;
+// 	} else {
+// 		formWrapper.classList.remove('is-active');
+// 	}
+//
+// });
