f198cdb1c056ddec412f2b27d49ec0eb8490d00d
Igor Goncharenko <igor.goncharenko86@gmail.com>
2020-03-10 17:23:39 +0200

    updates as for Moshev remarks

--- assets/js/landing-sign-up.js
+++ assets/js/landing-sign-up.js
@@ -1,17 +1,32 @@
-const isMobile = window.innerWidth < 600;
+const isMobile = window.innerWidth < 767;
 
 const letsGoBtn = document.querySelector('.lets-go-btn');
 const formWrapper = document.querySelector('.back-layer');
+const body = document.querySelector('body');
 let popupVisible = false;
 
 
 letsGoBtn.addEventListener('click', function() {
-
 	if (isMobile) {
 		formWrapper.classList.add('is-active');
+		body.classList.add('blocked');
 		popupVisible = true;
 	} else {
 		formWrapper.classList.remove('is-active');
+		body.classList.remove('blocked');
 	}
+});
 
+formWrapper.addEventListener('click', () => {
+	formWrapper.classList.remove('is-active');
+});
+
+const qualityCards = document.querySelectorAll('.quality-card');
+
+qualityCards.forEach(card => {
+	card.addEventListener('mouseover', () =>{
+		qualityCards.forEach(item => {
+			item.classList.remove('scaled-up');
+		})
+	})
 });
