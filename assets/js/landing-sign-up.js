963619cceabd7e5147bf936bdd3bb20fdb84b56d
Igor Goncharenko <igor.goncharenko86@gmail.com>
2020-03-11 11:01:32 +0200

    updates for Moshev 11.03.2020 landing-sign-up

--- assets/js/landing-sign-up.js
+++ assets/js/landing-sign-up.js
@@ -24,11 +24,32 @@ inner.addEventListener('click', () => {
 });
 
 const qualityCards = document.querySelectorAll('.quality-card');
+const scaledUpCard = document.querySelector('#carousel__slide2');
+let cardScaledUp = true;
 
 qualityCards.forEach(card => {
 	card.addEventListener('mouseover', () =>{
 		qualityCards.forEach(item => {
 			item.classList.remove('scaled-up');
-		})
-	})
+		});
+		cardScaledUp = false;
+	});
+	card.addEventListener('mouseout', () => {
+		scaledUpCard.classList.add('scaled-up');
+	});
+});
+
+
+const carouselItems = document.querySelectorAll('.carousel__navigation-button');
+let carouselButtonClicked= false;
+
+carouselItems.forEach(item => {
+
+	item.addEventListener('click', (e) => {
+		carouselItems.forEach(item => {
+			item.classList.remove('is-clicked');
+		});
+		e.target.classList.add('is-clicked');
+		carouselButtonClicked = true;
+	});
 });
