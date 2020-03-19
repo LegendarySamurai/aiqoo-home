ef43414be53e850f0b463d249a57f1ea4071db19
Igor Goncharenko <igor.goncharenko86@gmail.com>
2020-03-19 19:31:37 +0200

    home, landing, article, video refactoring done

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,75 +18,60 @@ const isDesktop = window.innerWidth >= 992;
 
 
 // Igor created 23 February 2020
-const headSection = document.querySelector('.head-section');
+// const headSection = document.querySelector('.head-section');
 // const headerSticky = document.querySelector('.header-sticky');
-const headerStickyHome = document.querySelector('.header-home');
+// const headerStickyHome = document.querySelector('.header-home');
 // const headerStickyLanding = document.querySelector('.header-landing');
 // const headerSticky = $('.header-sticky').not('.header-visible');
-let stickyHeaderHasBeenShown = false;
-let headerClientRect = headSection.getBoundingClientRect();
-
-const makeHeaderStickyHome = () => {
-	if (window.pageYOffset >= headerClientRect.bottom) {
-		headerStickyHome.classList.remove('is-hidden');
-		headerStickyHome.classList.add('is-active');
-		stickyHeaderHasBeenShown = true;
-	}
-	else if (window.pageYOffset < headerClientRect.bottom && stickyHeaderHasBeenShown) {
-		headerStickyHome.classList.add('is-hidden');
-	}
-};
-
-// const makeHeaderStickyLanding = () => {
+// let stickyHeaderHasBeenShown = false;
+// let headerClientRect = headSection.getBoundingClientRect();
+//
+// const makeHeaderStickyHome = () => {
 // 	if (window.pageYOffset >= headerClientRect.bottom) {
-// 		headerStickyLanding.classList.remove('is-hidden');
-// 		headerStickyLanding.classList.add('is-active');
+// 		headerStickyHome.classList.remove('is-hidden');
+// 		headerStickyHome.classList.add('is-active');
 // 		stickyHeaderHasBeenShown = true;
 // 	}
 // 	else if (window.pageYOffset < headerClientRect.bottom && stickyHeaderHasBeenShown) {
-// 		headerStickyLanding.classList.add('is-hidden');
+// 		headerStickyHome.classList.add('is-hidden');
 // 	}
 // };
 
 // window.addEventListener('scroll', function () {
-// 	makeHeaderStickyLanding();
+// 	if (isDesktop) {
+// 		makeHeaderStickyHome();
+// 	}
 // });
-
-window.addEventListener('scroll', function () {
-	if (isDesktop) {
-		makeHeaderStickyHome();
-	}
-});
-
-// Navbar mobile
-const navBarShowBtn = document.querySelector('#show-navbar');
-const navBarHideBtn = document.querySelector('#hide-navbar');
-const navBarCollapseBody = document.querySelector('#navbar');
-let havBarOpened = false;
-navBarHideBtn.classList.add('d-none');
-
-const showNavBar = () => {
-	navBarShowBtn.addEventListener('click', function() {
-		navBarCollapseBody.classList.remove('navbar-hide');
-		navBarCollapseBody.classList.add('navbar-active');
-		navBarHideBtn.classList.remove('d-none');
-		navBarHideBtn.classList.add('d-block');
-		havBarOpened = true;
-	});
-};
-
-const hideNavBar = () => {
-	navBarHideBtn.addEventListener('click', function() {
-		navBarCollapseBody.classList.remove('navbar-active');
-		navBarCollapseBody.classList.add('navbar-hide');
-		navBarHideBtn.classList.add('d-none');
-	});
-};
-
-
-if (isMobile) {
-	showNavBar();
-	hideNavBar();
-}
-
+//
+// // Navbar mobile
+// const navBarShowBtn = document.querySelector('#show-navbar');
+// const navBarHideBtn = document.querySelector('#hide-navbar');
+// const navBarCollapseBody = document.querySelector('#navbar');
+// let havBarOpened = false;
+// navBarHideBtn.classList.add('d-none');
+//
+// const showNavBar = () => {
+// 	navBarShowBtn.addEventListener('click', function() {
+// 		navBarCollapseBody.classList.remove('navbar-hide');
+// 		navBarCollapseBody.classList.add('navbar-active');
+// 		navBarHideBtn.classList.remove('d-none');
+// 		navBarHideBtn.classList.add('d-block');
+// 		havBarOpened = true;
+// 	});
+// };
+//
+// const hideNavBar = () => {
+// 	navBarHideBtn.addEventListener('click', function() {
+// 		navBarCollapseBody.classList.remove('navbar-active');
+// 		navBarCollapseBody.classList.add('navbar-hide');
+// 		navBarHideBtn.classList.add('d-none');
+// 	});
+// };
+//
+//
+// if (isMobile) {
+// 	showNavBar();
+// 	hideNavBar();
+// }
+//
 
