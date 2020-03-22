ad8d983f0e415802a15f28dc86b2d2e24511d961
Igor Goncharenko <igor.goncharenko86@gmail.com>
2020-03-22 21:33:29 +0200

    important updates and fixes

--- assets/js/home.js
+++ assets/js/home.js
@@ -3,17 +3,19 @@ const findBox = document.querySelector('.home .find-box');
 const findBoxRect = findBox.getBoundingClientRect();
 
 window.addEventListener('scroll', () => {
-	if (window.scrollY >= findBoxRect.top) {
-		if (!pageHeader.classList.contains('show-on-home')) {
-			pageHeader.classList.add('show-on-home');
+	if (window.innerWidth >= 992) {
+		if (window.scrollY >= findBoxRect.top) {
+			if (!pageHeader.classList.contains('show-on-home')) {
+				pageHeader.classList.add('show-on-home');
+			}
+			else if (pageHeader.classList.contains('hide-on-home')) {
+				pageHeader.classList.remove('hide-on-home');
+			}
 		}
-		else if (pageHeader.classList.contains('hide-on-home')) {
-			pageHeader.classList.remove('hide-on-home');
-		}
-	}
-	else {
-		if (pageHeader.classList.contains('show-on-home')) {
-			pageHeader.classList.add('hide-on-home');
+		else {
+			if (pageHeader.classList.contains('show-on-home')) {
+				pageHeader.classList.add('hide-on-home');
+			}
 		}
 	}
 });
