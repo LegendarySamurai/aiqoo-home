f0cfd12239391eda93001b08890dd9d5fe516453
Igor Goncharenko <igor.goncharenko86@gmail.com>
2020-06-12 18:19:57 +0300

    services page grid done. Css responsive files are inside regular ones.

--- assets/js/services-page.js
+++ assets/js/services-page.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
-	const services = document.querySelectorAll('.services-list-col');
-	console.log(services);
+	if (window.innerWidth > 600) {
+		const services = document.querySelectorAll('.services-list-col');
+		console.log(services);
 
-	services.forEach(service => {
-		const height = service.clientHeight;
-		// console.log(height);
-		const spans = Math.ceil(height / 10 + 1);
-		console.log(spans);
-		$(service).css({gridRowEnd: `span ${ spans+ 1}`});
-	});
+		services.forEach(service => {
+			const height = service.clientHeight;
+			// console.log(height);
+			const spans = Math.ceil(height / 10 + 1);
+			console.log(spans);
+			$(service).css({gridRowEnd: `span ${ spans+ 1}`});
+		});
+	}
+
 });
