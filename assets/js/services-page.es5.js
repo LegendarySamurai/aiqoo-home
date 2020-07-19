418a4d93ecfce5f45bd3310c3e53ec596ee49fa5
Igor Goncharenko <igor.goncharenko86@gmail.com>
2020-07-19 14:15:48 +0300

    in the main paga and all oter pages with this section we need to display the new boxs like in services

--- assets/js/services-page.es5.js
+++ assets/js/services-page.es5.js
@@ -3,13 +3,13 @@
 $(document).ready(function () {
 	if (window.innerWidth > 600) {
 		var services = document.querySelectorAll('.services-list-col');
-		console.log(services);
+		// console.log(services);
 
 		services.forEach(function (service) {
 			var height = service.clientHeight;
 			// console.log(height);
 			var spans = Math.ceil(height / 10 + 1);
-			console.log(spans);
+			// console.log(spans);
 			$(service).css({ gridRowEnd: 'span ' + (spans + 1) });
 		});
 	}
