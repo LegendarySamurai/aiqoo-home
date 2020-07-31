41cf5641bb11f74b4973d1bad342539a73178028
Igor Goncharenko <igor.goncharenko86@gmail.com>
2020-07-31 15:14:15 +0300

    scroll and active tab thing index business page done

--- assets/js/business-index.js
+++ assets/js/business-index.js
@@ -1,69 +1,100 @@
-
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-	anchor.addEventListener('click', function (e) {
-		e.preventDefault();
-
-		document.querySelector(this.getAttribute('href')).scrollIntoView({
-			behavior: 'smooth'
-		});
-	});
-});
-
-$(window).on('resize scroll', function() {
-	// console.log('999999999');
-
-	$('.section-content-b').each(function() {
-		let elementTop = $(this).offset().top;
-		// let elementBottom = $(window).height() - $(this).height();
-		// elementBottom = elementTop - elementBottom;
-		// console.log('1', this , elementTop);
-		// console.log('2',this,  elementBottom);
-
-		let elementHeight = $(this).innerHeight();
-		// console.log('=>', elementHeight);
-
-		let viewportTop = $(window).scrollTop();
-		// console.log('2', viewportTop);
-
-		let viewportBottom = viewportTop + $(window).height();
-		// console.log('3', viewportBottom);
-		if(window.outerHeight > (elementTop + elementHeight)) {
-			console.log('1', elementTop);
-			console.log('2', elementHeight);
-			console.log('3', window.outerHeight);
-			console.log(window.outerHeight > (elementTop + elementHeight));
-		}
-
-		// let positionOne = $('.about-us').getBoundingClientRect();
-		// console.log('a', $('.about-us').getClientRects);
-
-		// let elementBottom = viewportTop
-	});
-	// let elementOne = $('#a');
-	// let topOne = elementOne.offset().top;
-	// let bottomOne = elementOne.offset().bottom;
-	// console.log('a', topOne && bottomOne);
-});
-
-// $.fn.isInViewport = function() {
-// 	var elementTop = $(this).offset().top;
-// 	var elementBottom = elementTop + $(this).outerHeight();
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+// 	anchor.addEventListener('click', function (e) {
+// 		e.preventDefault();
 //
-// 	var viewportTop = $(window).scrollTop() - 500;
-// 	console.log();
-// 	var viewportBottom = viewportTop + $(window).height();
-//
-// 	return elementBottom > viewportTop && elementTop < viewportBottom;
-// };
-
-// $(window).on('resize scroll', function() {
-// 	$('.section-content-b').each(function() {
-// 		var activeTab = $(this).attr('id');
-// 		console.log('1', activeTab);
-// 		if ($(this).isInViewport()) {
-// 			$('#tab-' + activeTab).addClass(activeTab + '-active');
-// 		} else {
-// 			$('#tab-' + activeTab).removeClass(activeTab + '-active');
-// 		}
+// 		document.querySelector(this.getAttribute('href')).scrollIntoView({
+// 			behavior: 'smooth'
+// 		});
 // 	});
 // });
+
+$(window).on('load', function () {
+	const aboutUs = $('#about-us');
+	const ourProjects = $('#our-projects');
+	const ourReviews = $('#our-reviews');
+	const contentWrap = $('.content-wrap');
+
+	let aboutUsTop = aboutUs.offset().top;
+	let aboutUsHeight = aboutUs.innerHeight();
+	let aboutUsBottom = aboutUsTop + aboutUsHeight;
+
+	let ourProjectsTop = ourProjects.offset().top;
+	let ourProjectsHeight = ourProjects.innerHeight();
+	let ourProjectsBottom = ourProjectsTop + ourProjectsHeight;
+
+	let ourReviewsTop = ourReviews.offset().top;
+	let ourReviewsHeight = ourReviews.innerHeight();
+	let ourReviewsBottom = ourReviewsTop + ourReviewsHeight;
+
+	let contentWrapTop = contentWrap.offset().top;
+
+	$(window).on('resize scroll', function () {
+		let scroll = $(document).scrollTop();
+		let positionOnPage = contentWrapTop + scroll;
+		if (positionOnPage <= aboutUsBottom) {
+			// console.log('1', 'About us block');
+			$('#tab-a').addClass('active');
+			$('#tab-b').removeClass('active');
+			$('#tab-c').removeClass('active');
+		}
+		else if (positionOnPage > aboutUsBottom && positionOnPage <= ourProjectsBottom) {
+			// console.log('2', 'Our Projects block');
+			$('#tab-b').addClass('active');
+			$('#tab-a').removeClass('active');
+			$('#tab-c').removeClass('active');
+		}
+		else if (positionOnPage > ourProjectsBottom && positionOnPage <= ourReviewsBottom) {
+			// console.log('3', 'Our Reviews block');
+			$('#tab-c').addClass('active');
+			$('#tab-b').removeClass('active');
+			$('#tab-a').removeClass('active');
+		}
+	});
+
+	$('.tab').each(function () {
+		$(this).on('click', function () {
+			let target = $(this).attr('href');
+			console.log('target', target);
+			$('body,html').animate(
+				{
+					scrollTop: $(target).offset().top - contentWrapTop,
+					behavior: 'smooth'
+				},
+				800 //speed
+			);
+
+			$(this).addClass('active');
+		});
+	});
+
+	// $('#tab-a').click(function() {
+	// 	// $(window).scrollIntoView(aboutUsTop);
+	// 	$("body,html").animate(
+	// 		{
+	// 			scrollTop: $(aboutUsTop)
+	// 		},
+	// 		800 //speed
+	// 	);
+	// });
+
+	// $('#tab-b').click(function() {
+	// 	// $(window).scrollIntoView(aboutUsTop);
+	// 	$("body,html").animate(
+	// 		{
+	// 			scrollTop: $(ourProjectsTop)
+	// 		},
+	// 		800 //speed
+	// 	);
+	// });
+
+	// $('#tab-c').click(function() {
+	// 	// $(window).scrollIntoView(aboutUsTop);
+	// 	$("body,html").animate(
+	// 		{
+	// 			scrollTop: $(ourReviewsTop)
+	// 		},
+	// 		800 //speed
+	// 	);
+	// });
+});
+
